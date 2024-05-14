8c5fbc59fd8fd16196a4cf8c3d50752a59d54a02
asad <asadirfan939@gmail.com>
2024-05-14 10:18:22 +0500

    hello  am asad

---
+++ git.py
@@ -0,0 +1,13 @@
+# Function to add two numbers
+def add_numbers(num1, num2):
+    return num1 + num2
+
+# Taking input from user
+num1 = 33
+num2 = 12
+
+# Adding the numbers
+result = add_numbers(num1, num2)
+
+# Displaying the result
+print("The sum of {} and {} is: {}".format(num1, num2, result))
